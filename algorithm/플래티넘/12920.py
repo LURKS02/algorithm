11bd0d7bf4918cea52e7c8a121e78effbcea12f8
LURKS02 <thecrooks2@gmail.com>
2024-07-05 01:01:24 +0900

    12920

---
+++ algorithm/플래티넘/12920.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().rstrip().split())
+
+dp = [0] * (M+1)
+objects = []
+
+for _ in range(N):
+    W, C, K = map(int, input().split())
+
+    i = 1
+    while K > 0:
+        m = min(i, K)
+        objects.append((W * m, C * m))
+        K -= i
+        i *= 2
+
+for W, C in objects:
+    for i in range(M, W-1, -1):
+        dp[i] = max(dp[i], dp[i-W]+C)
+
+print(dp[M])
