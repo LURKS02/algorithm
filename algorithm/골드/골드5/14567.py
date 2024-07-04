6bdde07434bfbe14610f64853bfa1e23fe087b81
LURKS02 <thecrooks2@gmail.com>
2024-07-05 01:01:33 +0900

    14567

---
+++ algorithm/골드/골드5/14567.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+graph = [1] * (N+1)
+arr = [[] for _ in range(N+1)]
+
+for i in range(M):
+    a, b = map(int, input().split())
+    arr[a].append(b)
+
+for i in range(1, N+1):
+    for b in arr[i]:
+        graph[b] = max(graph[b], graph[i]+1)
+print(*graph[1:])
