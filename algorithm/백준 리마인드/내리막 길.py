035f2963204e5ea0dac3f6c1987dc36d1d9a20c7
LURKS02 <thecrooks2@gmail.com>
2025-02-09 10:02:52 +0900

    내리막 길

--- algorithm/백준 리마인드/내리막 길.py
+++ algorithm/백준 리마인드/내리막 길.py
@@ -2,19 +2,22 @@ import sys
 sys.setrecursionlimit(10**9)
 
 M, N = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(M)]
 
-answer = 0
+metrix = [list(map(int, input().split())) for _ in range(M)]
+dp = [[float('inf')] * N for _ in range(M)]
+
+visited = [[False] * N for _ in range(M)]
+
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
-dp = [[-float('inf')] * N for _ in range(M)]
 
-def dfs(x, y, matrix):
-    if dp[x][y] != -float('inf'):
+def dfs(x, y, visited):
+    if dp[x][y] != float('inf'):
         return dp[x][y]
 
     if x == M-1 and y == N-1:
         return 1
+
 
     dp[x][y] = 0
 
@@ -22,10 +25,12 @@ def dfs(x, y, matrix):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if 0 <= nx < M and 0 <= ny < N and matrix[nx][ny] < matrix[x][y]:
-            dp[x][y] += dfs(nx, ny, matrix)
+        if 0 <= nx < M and 0 <= ny < N and not visited[nx][ny] and metrix[x][y] > metrix[nx][ny]:
+            visited[nx][ny] = True
+            dp[x][y] += dfs(nx, ny, visited)
+            visited[nx][ny] = False
 
-    return max(dp[x][y], 0)
+    return dp[x][y]
 
-dfs(0, 0, matrix)
-print(dp[0][0])
+print(dfs(0, 0, visited))
+
