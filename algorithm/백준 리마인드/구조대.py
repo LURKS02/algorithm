4c1af9cdb4048d702a3a79f2460a4f41c7091df4
LURKS02 <thecrooks2@gmail.com>
2025-03-02 19:36:18 +0900

    구조대

--- algorithm/백준 리마인드/구조대.py
+++ algorithm/백준 리마인드/구조대.py
@@ -1,11 +1,4 @@
-from collections import defaultdict
-
 N, M = map(int, input().split())
-
-dic = defaultdict()
 
 for _ in range(N):
     l, r, c = map(int, input().split())
-    
-    dic[c].append((l, r))
-
